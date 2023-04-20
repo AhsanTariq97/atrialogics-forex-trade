1655a1e89d7e2e86be18c80a2591c55198e24363
AhsanTariq97 <a_tariq1997@yahoo.com>
2023-04-20 11:14:28 +0500

    design modification and sidebar functionality

--- src/compounds/Frame54Navbar.tsx
+++ src/compounds/Frame54Navbar.tsx
@@ -1,32 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Router from 'next/router';
 import Link from 'next/link';
 
+function useOutsideAlerter(ref: any, isVisible: boolean, setIsVisible: React.Dispatch<React.SetStateAction<boolean>>) {
+    useEffect(() => {
+      function handleClickOutside(event: { target: any; }) {
+        if (ref.current && !ref.current.contains(event.target) && isVisible) {
+          
+          setIsVisible(prev => !prev)
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref, isVisible, setIsVisible]);
+}
+
 const Frame54Navbar = ({ setActiveFrame54Tab }: {setActiveFrame54Tab: React.Dispatch<React.SetStateAction<string>>;}) => {
 
     const [navSidebar, setNavSidebar] = useState(false)
+      
+    const sidebarRef = useRef(null);
+
+    useOutsideAlerter(sidebarRef, navSidebar, setNavSidebar);
+    
 
   return (
         <nav className='flex items-center justify-between p-4 border-b'>
-            <Image src='/assets/icons/backarrow.svg' alt='' width={20} height={20} />
+            <Link href='/frame7'><Image src='/assets/icons/backarrow.svg' alt='' width={20} height={20} /></Link>
             <div className='items-center justify-between hidden pl-40 space-x-12 lg:flex'>
                 <div className='flex items-center justify-between space-x-1'>
                     <Image src='/assets/icons/unverified.svg' alt='' width={24} height={24} />
                     <h4>UNVERIFIED</h4>
                 </div>
-                <div className='flex items-center justify-between space-x-1'>
+                <Link href='/frame51'><div className='flex items-center justify-between space-x-1'>
                     <div className='bg-white rounded-full w-max'>
                         <Image src='/assets/icons/users.svg' alt='' width={24} height={24} />
                     </div>
                     <h4>BECOME AN IB</h4>
-                </div>
-                <div className='flex items-center justify-between space-x-1'>
+                </div></Link>
+                <Link href='/frame54'><div className='flex items-center justify-between space-x-1' onClick={() => setActiveFrame54Tab('COPY TRADING')}>
                     <div className='bg-white rounded-full w-max'>
                         <Image src='/assets/icons/users.svg' alt='' width={24} height={24} />
                     </div>
                     <h4>COPY TRADING</h4>
-                </div>
+                </div></Link>
                 <div className='flex items-center justify-between space-x-1'>
                     <Image src='/assets/icons/refresh.svg' alt='' width={24} height={24} />
                     <h4>REFRESH</h4>
@@ -39,12 +59,12 @@ const Frame54Navbar = ({ setActiveFrame54Tab }: {setActiveFrame54Tab: React.Disp
                 </div>
                 <Image src={`${navSidebar ? '/assets/icons/menuicon.svg' : '/assets/icons/menuicon.svg'}`} alt='' className='cursor-pointer' width={24} height={24} onClick={() => setNavSidebar(prev => !prev)} />
             </div>
-            <div className={`${navSidebar ? '' : 'hidden'} fixed z-10 right-0 px-4 py-4 top-[56.8px] bg-gradient-navSidebar`}>
+            <div ref={sidebarRef} className={`${navSidebar ? '' : 'hidden'} fixed z-20 right-0 px-4 py-4 top-[56.8px] bg-gradient-navSidebar`}>
                 <ul className='flex flex-col items-center justify-between'>
                     <li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]'>Profile</li>
                     <li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]'>Security</li>
-                    <li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]'>Become an IB</li>
-                    <li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]' onClick={() => setActiveFrame54Tab('COPY TRADING')}>Copy Trading</li>
+                    <Link href='/frame51'><li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]'>Become an IB</li></Link>
+                    <Link href='/frame54'><li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]' onClick={() => setActiveFrame54Tab('COPY TRADING')}>Copy Trading</li></Link>
                     <li className='cursor-pointer w-full text-center py-3 font-semibold px-6 border-b border-[#10B981]'>Restart Application</li>
                     <li className='w-full px-6 py-3 font-semibold text-center cursor-pointer'>Default</li>
                 </ul>
--- src/molecules/FavoriteTab.tsx
+++ src/molecules/FavoriteTab.tsx
@@ -6,14 +6,14 @@ const FavoriteTab = () => {
   return (
     <div className='flex flex-col items-center justify-between px-3'>
         <div className='grid items-center w-full grid-cols-2 space-y-2 xs:space-x-4 xs:space-y-0'>
-            <div className='flex items-center justify-center col-span-2 xs:col-span-1'>
-                <div className='flex items-center justify-start space-x-2 border-b-2 border-[#2F80ED] text-[#2F80ED] px-2 py-1'>
-                    <h3 className='text-sm'>FAVORITES</h3>
+            <div className='flex items-center justify-center col-span-2 xs:justify-start xs:col-span-1'>
+                <div className='flex items-center justify-start space-x-2 border-b-2 border-[#2F80ED] text-[#2F80ED] px-2 lg:px-0 xl:px-2 py-1'>
+                    <h3 className='text-sm lg:text-xs xl:text-sm'>FAVORITES</h3>
                     <FaStar />
                 </div>
-                <h3 className='border-b-2 border-[#D9D9D9] text-sm pl-4 py-1'>ALL SYMBOLS</h3>
+                <h3 className='border-b-2 border-[#D9D9D9] text-sm lg:text-xs xl:text-sm pl-4 lg:pl-2 xl:pl-4 py-1 whitespace-nowrap'>ALL SYMBOLS</h3>
             </div>
-            <div className='col-span-2 xs:col-span-1 border border-[#B4B4B4] rounded flex items-center px-1 space-x-1 max-w-xs mx-auto'>
+            <div className='col-span-2 xs:col-span-1 border border-[#B4B4B4] rounded flex items-center mx-auto px-1 space-x-1 max-w-xs overflow-x-hidden'>
                 <AiOutlineSearch />
                 <input type="text" placeholder='Search for asset to trade....' className='text-sm bg-transparent focus:outline-none' />
             </div>
